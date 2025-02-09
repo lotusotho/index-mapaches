94f17d917db16940d1d6a39993941f6894010a65
Alejandro Ramos <lotusotho11@gmail.com>
2025-02-09 17:30:12 +0100

    Change api url

--- src/app/services/status.service.ts
+++ src/app/services/status.service.ts
@@ -6,47 +6,48 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class StatusService {
+  private readonly API_URL = 'https://server.mapach.es';
+
   constructor(private httpClient: HttpClient) {}
 
   getAPIStatus(): Observable<any> {
-    return this.httpClient.get('https://server.mapach.es:4000/status', {
+    return this.httpClient.get(`${this.API_URL}/status`, {
       observe: 'response',
     });
   }
 
   getSSHStatus(): Observable<any> {
-    return this.httpClient.get('https://server.mapach.es:4000/status/ssh', {
+    return this.httpClient.get(`${this.API_URL}/status/ssh`, {
       observe: 'response',
     });
   }
 
   getPostgresStatus(): Observable<any> {
-    return this.httpClient.get(
-      'https://server.mapach.es:4000/status/postgres',
-      { observe: 'response' }
-    );
+    return this.httpClient.get(`${this.API_URL}/status/postgres`, {
+      observe: 'response',
+    });
   }
 
   getBlogAPIStatus(): Observable<any> {
-    return this.httpClient.get('https://server.mapach.es:4000/status/blogAPI', {
+    return this.httpClient.get(`${this.API_URL}/status/blogAPI`, {
       observe: 'response',
     });
   }
 
   getYachtStatus(): Observable<any> {
-    return this.httpClient.get('https://server.mapach.es:4000/status/yacht', {
+    return this.httpClient.get(`${this.API_URL}/status/yacht`, {
       observe: 'response',
     });
   }
 
   getEasyWGStatus(): Observable<any> {
-    return this.httpClient.get('https://server.mapach.es:4000/status/easyWG', {
+    return this.httpClient.get(`${this.API_URL}/status/easyWG`, {
       observe: 'response',
     });
   }
 
   getWebminStatus(): Observable<any> {
-    return this.httpClient.get('https://server.mapach.es:4000/status/webmin', {
+    return this.httpClient.get(`${this.API_URL}/status/webmin`, {
       observe: 'response',
     });
   }
