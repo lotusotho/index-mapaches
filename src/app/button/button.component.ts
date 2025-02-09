1cc2d613a35c0104502bbe5952265a2f94b45da6
Alejandro Ramos <lotusotho11@gmail.com>
2025-02-09 20:53:44 +0100

    Add css

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,10 +1,10 @@
-import { NgFor, NgIf } from '@angular/common';
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [NgFor, NgIf],
+  imports: [NgFor, NgIf, NgClass],
   templateUrl: './button.component.html',
   styleUrl: './button.component.css',
 })
